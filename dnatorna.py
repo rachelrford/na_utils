7f81f88cc46a2fee4d5e8365867e9e4a67b787d2
rachelrford <rford@caltech.edu>
2017-06-20 11:28:38 -0700

    Initial commit of dnatorna.py

---
+++ dnatorna.py
@@ -0,0 +1,38 @@
+"""
+Convert DNA sequences to RNA.
+"""
+
+def rna(seq):
+    """Convert a DNA sequence to RNA."""
+    #Determine if the original sequence was uppercase
+    seq_upper = seq.isupper()
+
+    #Convert to lowercase
+    seq = seq.lower()
+
+    #Swap out 't' for 'u'
+    seq = seq.replace('t','u')
+
+    #Return upper or lower, depending on input
+    if seq_upper:
+        return seq.upper()HOMEt as RNA"""
+    #Determine if the original sequence was uppercase
+    seq_upper = seq.isupper()
+
+    #Reverse sequence
+    seq = seq[::-1]
+
+    #Convert to uppercase
+    seq = seq.upper()
+
+    #Compute replacement
+    seq = seq.replace('A','u')
+    seq = seq.replace('T','a')
+    seq = seq.replace('G','c')
+    seq = seq.replace('C','g')
+
+    #Return in appropriate case
+    if seq_upper:
+        return seq.upper()
+    else:
+        return seq
